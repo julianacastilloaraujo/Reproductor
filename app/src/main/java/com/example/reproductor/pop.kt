91d65fa5729ac454999b4569f9f6edfc9f9d6326
jlianacastillo <lmov261220112@merida.tecnm.mx>
2023-03-22 12:22:02 -0600

    icono de guardar y compartir

---
+++ app/src/main/java/com/example/reproductor/pop.kt
@@ -0,0 +1,11 @@
+package com.example.reproductor
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class pop : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pop)
+    }
+}
