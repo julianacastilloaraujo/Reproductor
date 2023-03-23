894020bacd6298a3f4f9a8ecc374a13484a71d39
jlianacastillo <lmov261220112@merida.tecnm.mx>
2023-03-23 10:08:48 -0600

    se subio iconos completos con conexion de empty activity y se importaron los audios en raw

--- app/src/main/java/com/example/reproductor/MainActivity.kt
+++ app/src/main/java/com/example/reproductor/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.reproductor
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuInflater
 import android.view.MenuItem
 import android.widget.Toast
 
@@ -13,16 +15,26 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.menu, menu)
-        return super.onCreateOptionsMenu(menu)
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.menu, menu)
+        return true
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        when(item.itemId){
-            R.id.merengue -> Toast.makeText(this, "boton Merengue", Toast.LENGTH_SHORT).show()
-            R.id.salsa -> Toast.makeText(this, "boton Salsa", Toast.LENGTH_SHORT).show()
-            R.id.vallenato -> Toast.makeText(this, "boton Vallenato", Toast.LENGTH_SHORT).show()
+        return when (item.itemId) {
+            R.id.merengue -> {
+                startActivity(Intent(this, merengue::class.java))
+                true
+            }
+            R.id.salsa -> {
+                startActivity(Intent(this, salsa::class.java))
+                true
+            }
+            R.id.vallenato -> {
+                startActivity(Intent(this, vallenato::class.java))
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
         }
-        return super.onOptionsItemSelected(item)
     }
 }
---
+++ app/src/main/java/com/example/reproductor/vallenato.kt
@@ -0,0 +1,11 @@
+package com.example.reproductor
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class vallenato : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_vallenato)
+    }
+}
